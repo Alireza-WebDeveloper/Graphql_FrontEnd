6dfa6ee6c45122b04b29cca4a28aeec4d641598e
Alireza Khodadadi <alirezakh0199@gmail.com>
2024-06-02 11:46:47 +0330

    Add Custom Error

--- src/Page/Job/index.tsx
+++ src/Page/Job/index.tsx
@@ -4,8 +4,8 @@ import Detail from "../../Components/Home/job/Detail";
 
 const Page = () => {
   const { id }: any = useParams();
-  const { job } = useFetchGetJobById({ id });
-
+  const { job, error } = useFetchGetJobById({ id });
+  console.log(error);
   return <div>{job && <Detail item={job} />}</div>;
 };
 
